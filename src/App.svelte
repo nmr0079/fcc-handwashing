09d422375d4d05b2bbb76dea216e97b40bacb22e
nmr0079 <nmr98000@gmail.com>
2022-08-09 10:30:39 +0530

    committing html

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,15 @@
-<h1>Test website</h1>
+<style>
+	h1 {
+		text-align: center;
+	}
+</style>
+<script>
+	import Timer from './Timer.svelte';
+	import HowTo from './HowTo.svelte';
+</script>
+<h1>Hand Washing App</h1>
+
+<Timer />
+
+
+<HowTo />
---
+++ src/HowTo.svelte
@@ -0,0 +1,16 @@
+<style>
+    img {
+        max-width : 100%;
+    }
+    h3 {
+        text-align: center;
+    }
+</style>
+
+<div bp="grid">
+    <img bp="offset-5@md 4@md 12@sm" src="Hand-washing-1.png" alt="How to Wash your Hands">
+</div>
+
+<h3>
+    <a href="https://www.hey.nhs.uk/wp/wp-content/uploads/2017/01/Hand-washing-1.png">Picture Source</a>
+</h3>
